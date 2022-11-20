a9f915e649e1eb769ce914f417ca0dec09be5c64
Martin Cornejo <mart256@gmail.com>
2022-11-20 23:53:34 +0100

    Using test vector files

--- sim_1/new/DivideBy4FSM_TB.sv
+++ sim_1/new/DivideBy4FSM_TB.sv
@@ -2,8 +2,10 @@
 
 module DivideBy4FSM_TB;
 
-    logic clk, reset, y;
-     
+    logic clk, reset, y, yexpected;
+    logic testvectors[1000:0];
+    logic [31:0] vectornum, errors;
+
     DivideBy4FSM dut(
          .clk(clk),
          .reset(reset),
@@ -11,40 +13,30 @@ module DivideBy4FSM_TB;
     );
     
     initial begin
-        reset <= 1; #15;
-        assert(y === 1) else $error("Big problem");
-
-        reset <= 0; #10;
-        assert(y === 0) else $error("Big problem");
-
-        #10;
-        assert(y === 0) else $error("Big problem");
-
-        #10;
-        assert(y === 0) else $error("Big problem");
-
-        #10;
-        assert(y === 1) else $error("Big problem");
-
-        #10;
-        assert(y === 0) else $error("Big problem");
-
-        #10;
-        assert(y === 0) else $error("Big problem");
-
-        #10;
-        assert(y === 0) else $error("Big problem");
-
-        #10;
-        assert(y === 1) else $error("Big problem");
-
-        reset <= 1; #10;
-        assert(y === 1) else $error("Big problem");
+        $readmemb("DivideBy4FSM.tv", testvectors);
+        vectornum = 0; errors = 0;
+        reset <= 1; #15; reset <= 0;
     end
 
     always begin
         clk <= 1; #5;     
         clk <= 0; #5;     
     end
-    
+
+    always @(posedge clk) begin
+        #1; yexpected = testvectors[vectornum];
+    end
+
+    always @(negedge clk)
+        if (~reset) begin
+            if (y !== yexpected) begin
+                $display("Error: output = %b (%b expected)", y, yexpected);
+                $display("vectornum %b", vectornum);
+            end
+            vectornum = vectornum + 1;
+            if (testvectors[vectornum] === 'x) begin 
+                $display("%d tests completed with %d errors", vectornum, errors);
+                $finish;
+            end
+        end
 endmodule
